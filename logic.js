367831f7972920613aecd821bbfa7e908c6acace
Kanan <kenan@luxar.se>
2021-02-12 16:51:38 +0100

    remaked_GitHub

---
+++ logic.js
@@ -0,0 +1,45 @@
+async function getAllProducts(){
+    const result = await makeRequest("./api/recivers/productReciever.php", "GET")
+    //console.log(result)
+}
+
+
+async function deleteAllProducts(){
+
+    let body = new FormData()
+    body.append("action", "deleteAll")
+
+    const result = await makeRequest("./api/productReciever.php", "GET", body)
+    console.log(result)
+}
+
+
+async function addProduct(){
+
+    const product = {
+        name: document.getElementById("name").value,
+        price: document.getElementById("price").value,
+        size: document.getElementById("size").value
+    }
+
+    let body = new FormData()
+    body.append("action", "add")
+    body.append("product", JSON.stringify(product))
+
+    const result = await makeRequest("./api/productReciever.php", "POST", body)
+    console.log(result)
+}
+
+
+async function makeRequest(url, method, body){
+    try {
+        const response = await fetch(url, {
+            method, 
+            body
+        })
+        return response.json()
+    } catch(error) {
+        console.log(error)
+    }
+
+}
